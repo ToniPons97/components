6b3eb7e9341af7f41d73e8a9fd39e05356ca60df
Antonio Pons <toninopons@gmail.com>
2022-12-15 17:33:25 +0100

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Hello } from "./components/hello";
+import { Welcome } from "./components/welcome";
 
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Hello />
+                <Welcome name='Toni Pons' />
             </div>
         );
     }
