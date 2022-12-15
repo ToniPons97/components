9d061ed3aaf810c3a97de1b27fde0ef2d9a55358
Antonio Pons <toninopons@gmail.com>
2022-12-15 17:43:04 +0100

    First commit.

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export class Welcome extends React.Component {
+    static defaultProps = {
+        name: 'User'
+    }
+    
     render() {
         return <p>Welcome, {this.props.name}!</p>;
     }
