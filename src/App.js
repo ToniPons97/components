d29dd40e7c4ae04b55bfed5ed8e9438d721fe689
Antonio Pons <toninopons@gmail.com>
2022-12-15 17:09:51 +0100

    Created App.js in branch components-02, so I only moved the Hello components inside a div.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Hello } from "./components/hello";
 
 export class App extends React.Component {
     render() {
-        return <Hello />;
+        return (
+            <div>
+                <Hello />
+            </div>
+        );
     }
 }
