4d5c0d71d1b7d16f6dd6de6761043653d57bd6cd
Antonio Pons <toninopons@gmail.com>
2022-12-16 05:37:30 +0100

    It already had that behaviour.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome name={<strong>Toni Pons</strong>} age={25}/>
+                <Welcome name={<strong>Toni Pons</strong>} />
             </div>
         );
     }
