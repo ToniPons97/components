b0773447372c92e1557d7c15ca19b1b96600b11e
Antonio Pons <toninopons@gmail.com>
2022-12-16 05:21:00 +0100

    First commit.

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -11,7 +11,7 @@ export class Welcome extends React.Component {
         return (
             <div>
                 <p>Welcome, {this.props.name}!</p>
-                <Age age={this.props.age} />
+                {!!(this.props.age >= 18) && <Age age={this.props.age} />}
             </div>
         );
     }
