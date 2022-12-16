670803bc9b67b7b0ba53714f5ff20ae2fadc6e5f
Antonio Pons <toninopons@gmail.com>
2022-12-16 05:44:53 +0100

    First commit.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome name={<strong>Toni Pons</strong>} age={25}/>
+                <Welcome name='John' age={25}/>
             </div>
         );
     }
