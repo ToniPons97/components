0ac7090bb0988b68117cc8f9a4f269179248f0d9
Antonio Pons <toninopons@gmail.com>
2022-12-23 06:18:54 +0100

    Done.

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -9,7 +9,7 @@ export class Welcome extends React.Component {
     render() {
 
         return (
-            <div>
+            <div className="welcome">
                 <p>Welcome, {this.props.name}!</p>
                 {this.props.name === 'John' && <Age age={this.props.age} />}
             </div>
